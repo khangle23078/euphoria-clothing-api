00af9fcf619114d5268433cb9744c4a5b4841301
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2024-04-12 08:41:05 +0700

    feat: delete file

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { BAD_REQUEST, OK } from "http-status"
+import { v2 as cloudinary } from 'cloudinary'
 
 export const uploadSingleFile = (req: Request, res: Response) => {
   try {
@@ -11,6 +12,22 @@ export const uploadSingleFile = (req: Request, res: Response) => {
         url: image?.path,
         public_id: image?.filename
       }
+    })
+  } catch (error) {
+    return res.status(BAD_REQUEST).json({
+      status: BAD_REQUEST,
+      message: error
+    })
+  }
+}
+
+export const deleteFile = async (req: Request, res: Response) => {
+  try {
+    const { public_id } = req.body
+    await cloudinary.uploader.destroy(public_id)
+    return res.status(OK).json({
+      status: OK,
+      message: 'Delete file success'
     })
   } catch (error) {
     return res.status(BAD_REQUEST).json({
--- src/routes/upload.route.ts
+++ src/routes/upload.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { upload } from "../configs/cloudinary.config";
-import { uploadSingleFile } from "../controllers/upload.controller";
+import { deleteFile, uploadSingleFile } from "../controllers/upload.controller";
 
 const route = Router()
 
 route.post('/upload/single', upload.single('image'), uploadSingleFile)
+route.post('/delete', deleteFile)
 
 export default route
