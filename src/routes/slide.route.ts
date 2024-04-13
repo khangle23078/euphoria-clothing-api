f4e7763d3937ddb0578bf2cbca8017236eb9a6c0
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2024-04-13 15:42:17 +0700

    add deleteSlide to slide route

--- src/routes/slide.route.ts
+++ src/routes/slide.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createslide, getSlides, updateSlide } from "../controllers/slide.controller";
+import { createslide, deleteSlide, getSlides, updateSlide } from "../controllers/slide.controller";
 
 const route = Router()
 
 route.get('/', getSlides)
 route.post('/', createslide)
 route.put('/:id', updateSlide)
-route.delete('/:id',)
+route.delete('/:id', deleteSlide)
 
 export default route
